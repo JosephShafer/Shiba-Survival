b5d8a81a2750cab3d429fc879754a69d094f5569
Amber Zaragoza <amberzaragoza95@gmail.com>
2019-02-14 11:05:09 -0800

    added personal file

---
+++ amberZ.cpp
@@ -0,0 +1,46 @@
+// Program: amberZ.cpp
+// Author:  Amber Zaragoza
+// Personal functions for Shiba Survival
+
+#include <iostream>
+#include <cstdlib>
+#include <cstring>
+#include <unistd.h>
+#include <ctime>
+#include <cmath>
+#include <X11/Xlib.h>
+#include <X11/keysym.h>
+#include <GL/glx.h>
+#include "log.h"
+#include "fonts.h"
+
+void displayName(int x, int y)
+{
+    unsigned int c = 0x00ffffff;
+    ggprint8b(&r[i], 16, c, "Requirements");
+}
+
+void displayPicture(int x, int y, GLuint textid)
+{
+    glColor3ub(255, 255, 255);
+    static float angel = 0.0f;
+    static int wid = 30;
+    wid += sin(angle) * 10;
+    float fx = (float)x;
+    float fy = (float)y;
+    float a = 0;
+    a += sin(angle) * 10.0f;
+    fx += a;
+    angle += 0.2f;
+    glPushMatrix();
+    glTranslatef(fx, fy, 0);
+    glRotatef(a, 0, 0, 1.0);
+    glBindTexture(GL_TEXTURE_2D, textid);
+    glBegin(GL_QUADS);
+        glTexCoord2f(0.0f, 1.0f); glVertex2i(-wid, -wid);
+        glTexCoord2f(0.0f, 0.0f); glVertex2i(-wid,  wid);
+        glTexCoord2f(1.0f, 1.0f); glVertex2i( wid,  wid);
+        glTexCoord2f(1.0f, 1.0f); glVertex2i( wid, -wid);
+    glEnd();
+    glPopMatrix();
+}

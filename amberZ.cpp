3f566ceb99303ac57fe88e8e0cb4b190fd50752a
Amber Zaragoza <amberzaragoza95@gmail.com>
2019-02-16 13:32:47 -0800

    removed unnecessary includes

--- amberZ.cpp
+++ amberZ.cpp
@@ -2,16 +2,7 @@
 // Author:  Amber Zaragoza
 // Personal functions for Shiba Survival
 
-#include <iostream>
-#include <cstdlib>
-#include <cstring>
-#include <unistd.h>
-#include <ctime>
-#include <cmath>
-#include <X11/Xlib.h>
-#include <X11/keysym.h>
 #include <GL/glx.h>
-#include "log.h"
 #include "fonts.h"
 
 void amberZ(int x, int y, GLuint textid)
@@ -20,7 +11,7 @@ void amberZ(int x, int y, GLuint textid)
     float wid = 120.0f;
     glColor3ub(255, 255, 255);
     glPushMatrix();
-    glTranslatef((float)x, (float)y, 0);
+    glTranslatef((float) x, (float) y, 0);
     glBindTexture(GL_TEXTURE_2D, textid);
     glBegin(GL_QUADS);
     glTexCoord2f(0.0f, 1.0f); glVertex2i(-wid/2, -wid/2);

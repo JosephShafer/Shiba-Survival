cff492f671e959c1f8f52be85bc0d5ef76c9350b
JosephShafer <joeyshafer96@gmail.com>
2019-02-15 20:55:40 -0800

    Made my picture less extra

--- josephS.cpp
+++ josephS.cpp
@@ -16,24 +16,24 @@
 void josephS(int x, int y, GLuint textid)
 {
     Rect name;
-    name.left = x + 200;
+    name.left = x + 90;
     name.bot = y;
     name.center = 0;
     ggprint16(&name, 16, 0x00ffff00, "Joseph Shafer");
        
     glColor3ub(225,225,225);
-    static float angle = 0.0f;
-    static int wid = 30;
-    wid += sin(angle) * 10;
+    //static float angle = 0.0f;
+    float wid = 60.0f;
+    //wid += sin(angle) * 10;
     float fx = (float)x;
     float fy = (float)y;
-    float a = 0;
-    a += sin(angle) * 10.0f;
-    fx += a;
-    angle += 0.2f;
+    //float a = 0;
+    //a += sin(angle);
+    //fx += a;
+    //angle += 0.2f;
     glPushMatrix();
     glTranslatef(fx, fy, 0);
-    glRotatef(a, 0, 0, 1.0);
+    //glRotatef(a, 0, 0, 1.0);
     glBindTexture(GL_TEXTURE_2D, textid);
     glBegin(GL_QUADS);
         glTexCoord2f(0.0f, 1.0f); glVertex2i(-wid, -wid);

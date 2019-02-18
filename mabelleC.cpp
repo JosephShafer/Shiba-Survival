7ed31ea9a6b62db3f22c0518a00214a63ca6cf8c
mab <mabellecruz@gmail.com>
2019-02-18 14:22:48 -0800

    updated personal file

--- mabelleC.cpp
+++ mabelleC.cpp
@@ -1,35 +1,31 @@
-//Mabelle Cruz
+// Program: mabelleC.cpp
+// Author: Mabelle Cruz
+// CS3350: Spring 2019
+// Project: Shiba-Survival
 
-#include <iostream>
-#include <cstdlib>
-#include <cstring>
-#include <unistd.h>
-#include <ctime>
-#include <cmath>
-#include <X11/Xlib.h>
-#include <X11/keysym.h>
 #include <GL/glx.h>
-#include "log.h"
 #include "fonts.h"
 
 void mabelleC(int x, int y, GLuint id)
 {
-    float wid = 120.0f;
-    Rect r;
-    
-    glColor3ub(255, 255, 255);
-    glPushMatrix();
-    glTranslatef((float)x, (float)y, 0);
-    glBindTexture(GL_TEXTURE_2D, id);
-    glBegin(GL_QUADS);
-    glTexCoord2f(0.0f, 1.0f); glVertex2i(-wid/2, -wid/2);
-    glTexCoord2f(0.0f, 0.0f); glVertex2i(-wid/2,  wid/2);
-    glTexCoord2f(1.0f, 0.0f); glVertex2i( wid/2,  wid/2);
-    glTexCoord2f(1.0f, 1.0f); glVertex2i( wid/2, -wid/2);
-    glEnd();
-    glPopMatrix();
-    r.bot = y;
-    r.left = x + 90;
-    r.center = 0;
-    ggprint16(&r, 16, 0x00ffff00, "Mabelle Cruz");
+	Rect r;
+	float wid = 120.0f;
+
+	// Displays my name
+	r.bot = y;
+	r.left = x + 90;
+	r.center = 0;
+	ggprint16(&r, 16, 0x00ffff00, "Mabelle Cruz");
+	// Displays my picture
+	glColor3ub(255, 255, 255);
+	glPushMatrix();
+	glTranslatef((float)x, (float)y, 0);
+	glBindTexture(GL_TEXTURE_2D, id);
+	glBegin(GL_QUADS);
+	glTexCoord2f(0.0f, 1.0f); glVertex2i(-wid/2, -wid/2);
+	glTexCoord2f(0.0f, 0.0f); glVertex2i(-wid/2,  wid/2);
+	glTexCoord2f(1.0f, 0.0f); glVertex2i( wid/2,  wid/2);
+	glTexCoord2f(1.0f, 1.0f); glVertex2i( wid/2, -wid/2);
+	glEnd();
+	glPopMatrix();
 }

a8f8009c3e20dfb457d43b2cfe402dfd3b44aa0e
JosephShafer <joeyshafer96@gmail.com>
2019-04-09 15:24:32 -0700

    Fixed window size

--- shiba.cpp
+++ shiba.cpp
@@ -340,7 +340,7 @@ public:
 		//it will undo the last change done by XDefineCursor
 		//(thus do only use ONCE XDefineCursor and then XUndefineCursor):
 	}
-} x11(0, 0);
+} x11(1366, 768);
 
 //function prototypes
 void init_opengl(void);

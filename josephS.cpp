a77a9a6fb8e8c2ca9d32bddf971712e891db1ac9
JosephShafer <joeyshafer96@gmail.com>
2019-05-02 13:27:21 -0700

    Refactored and cleaned out old code

--- josephS.cpp
+++ josephS.cpp
@@ -7,16 +7,6 @@
 JoeyGlobal *JoeyGlobal::instance = 0;
 JoeyGlobal *JSglobalVars = JSglobalVars->getInstance();
 
-//typedef float Vec[3];
-
-/*
-Ideas related to enemies:
-    1. move toward the ship at different rates
-    2. Will Have different sprites for different enemys
-    3. Unlike asteroids, needs to be continually created. Shouldn't exceed a max number. 
-    4. Could make a max number of enemies depending on level?
-*/
-
 void enemyGetResolution(float Xres, float Yres)
 {
 	JSglobalVars->gameXresolution = Xres;
@@ -31,21 +21,12 @@ garden hoses
 spray bottles
 flea/bugs
 other dogs
-
-
 chocolate?
 */
 
 //=============================================================
 //Enemy functions
 //=============================================================
-
-
-/*
-Never want to spawn on top of ship, would not be fair to player
-get shiba position, 
-can't spawn past edge, 
-*/
 
 vector<Enemy> enemies;
 Lives numLivesLeft;
@@ -62,23 +43,22 @@ Enemy::Enemy(float shibaXPosition, float shibaYPosition)
 	health = 100;
 	speed = .01;
 	
-
 	//enemies spawn
 	int spawnchoice = (rand() % 4);
 	int spaceAway = 100;
 	bool enemySpawned = false;
 
 	//spawn top right area of game area
-	while(!enemySpawned){
-		if (spawnchoice == 0){
-			if((shibaXPosition + spaceAway) < JSglobalVars->gameXresolution && (shibaYPosition + spaceAway) < JSglobalVars->gameXresolution){
+	while(!enemySpawned) {
+		if (spawnchoice == 0) {
+			if((shibaXPosition + spaceAway) < JSglobalVars->gameXresolution && (shibaYPosition + spaceAway) < JSglobalVars->gameXresolution) {
 				position[0] = (rand() % ((int)(JSglobalVars->gameXresolution - spaceAway + 1 - shibaXPosition)))
 								+ shibaXPosition + spaceAway;
 				position[1] = (rand() % ((int)(JSglobalVars->gameXresolution - spaceAway + 1 - shibaYPosition)))
 								+ shibaYPosition + spaceAway;
 				enemySpawned = true;
 				#ifdef DEBUG
-					if(position[0] > JSglobalVars->gameXresolution || position[1] > JSglobalVars->gameXresolution){
+					if(position[0] > JSglobalVars->gameXresolution || position[1] > JSglobalVars->gameXresolution) {
 						printf("Top right Error: %f, %f\n", position[0], position[1]);
 					}
 				#endif
@@ -88,14 +68,14 @@ Enemy::Enemy(float shibaXPosition, float shibaYPosition)
 			}
 		}
 		//spawn top left area of shiba
-		if (spawnchoice == 1){
-			if((shibaXPosition) > 0 && (shibaYPosition + spaceAway) < JSglobalVars->gameXresolution){
+		if (spawnchoice == 1) {
+			if((shibaXPosition) > 0 && (shibaYPosition + spaceAway) < JSglobalVars->gameXresolution) {
 				position[0] = (rand() % ((int)(shibaXPosition - spaceAway + 1)));
 				position[1] = (rand() % ((int)(JSglobalVars->gameXresolution - spaceAway + 1 - shibaYPosition)))
 								+ shibaYPosition + spaceAway;
 				enemySpawned = true;
 				#ifdef DEBUG
-					if(position[0] < 0 || position[1] > JSglobalVars->gameXresolution){
+					if(position[0] < 0 || position[1] > JSglobalVars->gameXresolution) {
 						printf("Top Left Error: %f, %f\n", position[0], position[1]);
 					}
 				#endif
@@ -106,14 +86,14 @@ Enemy::Enemy(float shibaXPosition, float shibaYPosition)
 
 		// rand() % (max_number + 1 - minimum_number) + minimum_number
 		// bottom right
-		if (spawnchoice == 2){
-			if((shibaXPosition + spaceAway) < JSglobalVars->gameXresolution && (shibaYPosition > 0)){
+		if (spawnchoice == 2) {
+			if((shibaXPosition + spaceAway) < JSglobalVars->gameXresolution && (shibaYPosition > 0)) {
 				position[0] = (rand() % ((int)(JSglobalVars->gameXresolution - spaceAway + 1 - shibaXPosition)))
 								+ shibaXPosition + spaceAway;
 				position[1] = (rand() % ((int)(shibaYPosition - spaceAway + 1 )));
 				enemySpawned = true;
 				#ifdef DEBUG
-					if(position[0] > JSglobalVars->gameXresolution || position[1] < 0){
+					if(position[0] > JSglobalVars->gameXresolution || position[1] < 0) {
 						printf("Bottom Right Error: %f, %f\n", position[0], position[1]);
 					}
 				#endif
@@ -123,13 +103,13 @@ Enemy::Enemy(float shibaXPosition, float shibaYPosition)
 		}
 
 		// bottom left
-		if (spawnchoice == 3){
-			if((shibaXPosition - spaceAway) > 0 && (shibaYPosition - spaceAway > 0)){
+		if (spawnchoice == 3) {
+			if((shibaXPosition - spaceAway) > 0 && (shibaYPosition - spaceAway > 0)) {
 				position[0] = (rand() % ((int)(shibaXPosition - spaceAway + 1)));
 				position[1] = (rand() % ((int)(shibaYPosition - spaceAway + 1 )));
 				enemySpawned = true;
 			#ifdef DEBUG
-				if(position[0] < 0 || position[1] < 0){
+				if(position[0] < 0 || position[1] < 0) {
 					printf("Bottom Left Error: %f, %f\n", position[0], position[1]);
 				}
 			#endif
@@ -140,22 +120,14 @@ Enemy::Enemy(float shibaXPosition, float shibaYPosition)
 	}	//end while
 };
 
-
-
 void Enemy::drawEnemy()
 {
-        // square until we add sprites
-         //sideLength = 10.0f;
-                
-
-       
         drawSprite(JSglobalVars->doctorTextureID, enemy, sideLength, sideLength, position[0], position[1]);
 }
 
 void Enemy::updatePosition(float shibaXposition, float shibaYposition, int indexOfEnemy)
 {
 	
-
 	if(position[0] < shibaXposition)
 		velocity[0] += speed;
 	if(position[0] > shibaXposition)
@@ -168,29 +140,30 @@ void Enemy::updatePosition(float shibaXposition, float shibaYposition, int index
 	position[0] = position[0] + velocity[0];
 	position[1] = position[1] + velocity[1];
 	
-	if((position[0] - sideLength) <= 0){
+	if((position[0] - sideLength) <= 0) {
 		position[0] = sideLength;
 		velocity[0] *= -1;
 	}
-	if((position[0] + sideLength) >= JSglobalVars->gameXresolution){
+	if((position[0] + sideLength) >= JSglobalVars->gameXresolution) {
 		position[0] = JSglobalVars->gameXresolution - sideLength;
 		velocity[0] *= -1;
 	}
-	if((position[1] - sideLength) <= 0){
+	if((position[1] - sideLength) <= 0) {
 		position[1] = sideLength;
 		velocity[1] *= -1;
 	}
-	if((position[1] + sideLength) >= JSglobalVars->gameYresolution){
+	if((position[1] + sideLength) >= JSglobalVars->gameYresolution) {
 		position[1] = JSglobalVars->gameYresolution - sideLength;
 		velocity[1] *= -1;
 	}
 
-	
-	//weird formatting just makes it easier for me to see for now
-	if(     (((position[0] - sideLength) < shibaXposition)  && ((position[0] + sideLength) > shibaXposition))
-															  &&
-			(((position[1] - sideLength) < shibaYposition)  && ((position[1] + sideLength) > shibaYposition))
-	) {
+	if((( (position[0] - sideLength) < shibaXposition)
+		  && 
+		  ((position[0] + sideLength) > shibaXposition))
+			&&
+			(((position[1] - sideLength) < shibaYposition)
+			  && 
+			  ((position[1] + sideLength) > shibaYposition))) {
 		shibaCollision(indexOfEnemy);
 		numLivesLeft.changeLives(-1);
 	}
@@ -224,43 +197,32 @@ void Enemy::takeDamage(int damageDone)
 //		Score Display
 //=============================================================
 
-/**
- * What I need:
- * Display the Score
- * Update the score in main file
- * */
-
 void textScoreDisplay()
 {
 	Rect score;
     score.left = JSglobalVars->gameXresolution * .80;
     score.bot = JSglobalVars->gameYresolution * .95;
     score.center = 0;
-	//static double i = 1.0;
-	//i += scoreIncrease;
     ggprint16(&score, 16, 0x00ffff00, "Score: %019.0f", scoreObject.getScore());
-	//used in bullet hit enemy
 }
 
-// void scoreCalculator(float enemySize, float * score)
-// {
-// 	float scoreIncrease = 10000*(1/enemySize);
-// 	*score += scoreIncrease;
-// }
-
-Lives::Lives(){
+Lives::Lives()
+{
 	currentLives= 3;
 };
 
-int Lives::getLives(){
+int Lives::getLives()
+{
 	return currentLives;
 };
 
-void Lives::setLives(int changeValue){
+void Lives::setLives(int changeValue)
+{
 	currentLives = changeValue;
 };
 
-void Lives::changeLives(int difference){
+void Lives::changeLives(int difference)
+{
 	currentLives += difference;
 };
 
@@ -269,15 +231,18 @@ Score::Score()
 	currentScore = 0;
 };
 
-float Score::calculateScore(float scoreIncrease){
+float Score::calculateScore(float scoreIncrease)
+{
 	return 100000*(1/scoreIncrease);
 };
 
-void Score::setScore(float scoreSet){
+void Score::setScore(float scoreSet)
+{
 	currentScore += scoreSet;
 };
 
-float Score::getScore(){
+float Score::getScore()
+{
 	return currentScore;
 };
 
@@ -293,13 +258,12 @@ void livesTextDisplay()
 // Functions used in Main file
 //=============================================================
 
-
-
 void createEnemy(int numToCreate, float shibaXPosition, float shibaYPosition)
 {
 	for(int i = 0; i < numToCreate; i++)
 		enemies.push_back(Enemy(shibaXPosition, shibaYPosition));
 }
+
 // destroys an element in a vector by it's index
 void destroyEnemy(int index)
 {
@@ -310,13 +274,8 @@ void destroyEnemy(int index)
 void renderEnemies()
 {
 	for(unsigned int i = 0; i < enemies.size(); i++) {
-	glPushMatrix();
-		//glTranslated(enemies[i].position[0], enemies[i].position[1], 0);
-		//printf(" %d: %f %f\n", i, enemies[i].position[0], enemies[i].position[1]);
-		//enemies[i].updatePosition(g.ship.pos[0], g.ship.pos[1], gl->xres, gl->yres);
 		enemies[i].drawEnemy();
 		updateFrame(enemy);
-	glPopMatrix();
 	}
 }
 
@@ -325,7 +284,7 @@ void updateAllPosition(float shibaXposition, float shibaYposition)
 	for(unsigned int i = 0; i < enemies.size(); i++) {
 		enemies[i].updatePosition(shibaXposition, shibaYposition, i);
 		// TODO MOVE TO OTHER FUNCTION MAYBE
-		if(enemies[i].health < 1){
+		if(enemies[i].health < 1) {
 			destroyEnemy(i);
 		}
 		// END TODO
@@ -334,7 +293,7 @@ void updateAllPosition(float shibaXposition, float shibaYposition)
 
 void cleanupEnemies()
 {
-	while(enemies.size() != 0){
+	while(enemies.size() != 0) {
 		enemies.pop_back();
 	}
 }
@@ -342,7 +301,7 @@ void cleanupEnemies()
 bool bulletHitEnemy(float bulletX, float bulletY)
 {
 	bool hit = false;
-	for(unsigned int j = 0; j < enemies.size(); j++){
+	for(unsigned int j = 0; j < enemies.size(); j++) {
 			if((bulletX - enemies[j].sideLength < enemies[j].position[0]) 
 				&&
 				(bulletX + enemies[j].sideLength > enemies[j].position[0])
@@ -354,7 +313,6 @@ bool bulletHitEnemy(float bulletX, float bulletY)
 				{
 				enemies[j].takeDamage(100);
 				hit = true;
-				// TODO FIX COUPLING?
 				scoreObject.setScore(scoreObject.calculateScore(enemies[j].sideLength));
 			}
 		}
@@ -371,7 +329,7 @@ void primeSpawner(int milliseconds, float shibaXposition, float shibaYposition)
 	int spawnChecker = (milliseconds % primeArray[currentIndex]);
 	unsigned int enemyCap = 10;
 	
-	if(spawnChecker == 0 && enemies.size() < enemyCap){
+	if(spawnChecker == 0 && enemies.size() < enemyCap) {
 		createEnemy(numToMake, shibaXposition, shibaYposition);
 		if (currentIndex > 0) {
 			--currentIndex;
@@ -381,14 +339,14 @@ void primeSpawner(int milliseconds, float shibaXposition, float shibaYposition)
 		}
 	}
 
-	if(milliseconds % 401 == 0){
+	if(milliseconds % 401 == 0) {
 		enemyCap++;
 	}
 
 }
 
 
-void getDoctorTextureFunction(GLuint recievedTexture){
+void getDoctorTextureFunction(GLuint recievedTexture) {
 	 
 		JSglobalVars->doctorTextureID = recievedTexture;
 }
@@ -425,32 +383,4 @@ void josephS(float x, float y, GLuint textid)
     glEnd();
     glPopMatrix();
 }
-
-/**
- * 
-Image enemySprite[1] = {"possibleEnemy.gif"};
-GLuint EnemyTexture;
-
-void enemyInit_openGL(){
-	int w = enemySprite[0].width;
-	int h = enemySprite[0].height;
-	glBindTexture(GL_TEXTURE_2D, EnemyTexture);
-	//
-	glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_NEAREST);
-	glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_NEAREST);
-	//
-	//must build a new set of data...
-	unsigned char *EnemyData = buildAlphaData(&enemySprite[0]);	
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, w, h, 0,
-		GL_RGBA, GL_UNSIGNED_BYTE, EnemyData);
-}
-
-
-// void createEnemies(vector<Enemy> &EnemyInstance, int VecSize){
-// 	for(int i = 0; i < VecSize; i++){
-// 		EnemyInstance.push_back(Enemy());
-
-// }
-*/
-
 
